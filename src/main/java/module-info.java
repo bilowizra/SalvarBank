7326fef715c04abc590905b6a5b9b16d6644add1
bilowizra <74020331+bilowizra@users.noreply.github.com>
2023-05-14 18:07:43 +0300

    Test Commit 1.2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,15 @@
 module com.bankapplication.salvarbank {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.bankapplication.salvarbank to javafx.fxml;
     exports com.bankapplication.salvarbank;
+    exports com.bankapplication.salvarbank.Controllers;
+    exports com.bankapplication.salvarbank.Controllers.Admin;
+    exports com.bankapplication.salvarbank.Controllers.Client;
+    exports com.bankapplication.salvarbank.Models;
+    exports com.bankapplication.salvarbank.Views;
 }
