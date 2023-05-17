e527e9d151c4b9d9452d5e13c116ebdb4b4265b9
bilowizra <74020331+bilowizra@users.noreply.github.com>
2023-05-17 14:20:19 +0300

    First starting the App and checking the Login UI

--- src/main/java/com/bankapplication/salvarbank/Controllers/LoginController.java
+++ src/main/java/com/bankapplication/salvarbank/Controllers/LoginController.java
@@ -1,4 +1,15 @@
 package com.bankapplication.salvarbank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label payee_address_lbl;
+    public TextField payee_address_fld;
+    public TextField password_fld;
+    public Button login_btn;
+    public Label error_lbl;
 }
